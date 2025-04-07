2abe28c5c364c8283a2debf557ed694ec73f7ae1
Ritik2004 <hritikgaur44@gmail.com>
2025-04-07 23:42:08 +0530

    seen and is typing added

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,12 @@ import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.routes.js"
 import cors from "cors";
 import {app,server} from './lib/socket.js'
-
+import path from "path"
 dotenv.config()
 
 
 const PORT = process.env.PORT
-
+const __dirname = path.resolve()
 app.use(express.json());
 app.use(cookieParser());
 
@@ -22,6 +22,14 @@ app.use(cors({
 ))
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+}
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
 
 
 server.listen(PORT, ()=>{
